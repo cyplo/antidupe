a1e978415c2eb9019a321a74cb653795c026ce99
Cyryl Płotnicki <cyplo@cyplo.net>
2017-10-19 12:43:46 +0100

    Basic trait for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+mod antidupe {
+    use std::path::Path;
+
+    trait InputPath {}
+
+    impl InputPath for Path {}
+
+    #[cfg(test)]
+    mod antidupe_should_return {
+        #[test]
+        fn no_duplicates_for_empty_directory() {}
+    }
+}
