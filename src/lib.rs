ea1ad666c4df0b7215e075ea007fd4926a87c185
Cyryl Płotnicki <cyplo@cyplo.net>
2017-10-19 12:45:08 +0100

    Move the library code to the separate file

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod antidupe {
+    use std::path::Path;
+
+    trait InputPath {}
+
+    impl InputPath for Path {}
+
+    #[cfg(test)]
+    mod antidupe_should_return {
+        #[test]
+        fn no_duplicates_for_empty_directory() {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,3 @@ fn main() {
 }
 
 
-mod antidupe {
-    use std::path::Path;
-
-    trait InputPath {}
-
-    impl InputPath for Path {}
-
-    #[cfg(test)]
-    mod antidupe_should_return {
-        #[test]
-        fn no_duplicates_for_empty_directory() {}
-    }
-}
