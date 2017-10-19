2ecc4b8de49c0e3490a5ff966c16afe256ae23ad
Cyryl Płotnicki <cyplo@cyplo.net>
2017-10-19 15:18:03 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,42 @@
 mod antidupe {
+    pub trait DirectoryContentsLister {
+        
+    }
+
     use std::path::Path;
 
-    trait InputPath {}
+    pub struct DuplicatedItem {
+        path: Path
+    }
 
-    impl InputPath for Path {}
+    use std::iter::empty;
 
-    #[cfg(test)]
-    mod antidupe_should_return {
-        #[test]
-        fn no_duplicates_for_empty_directory() {}
+    pub fn get_duplicates<'a>(lister: &'a DirectoryContentsLister) -> Iterator<Item = &'a DuplicatedItem> {
+        empty::<&DuplicatedItem>();
+    }
+
+}
+
+#[cfg(test)]
+mod antidupe_should_report {
+
+    use antidupe::DirectoryContentsLister;
+    use antidupe::get_duplicates;
+
+    struct FakeDirectoryContentsLister { }
+
+    impl FakeDirectoryContentsLister {
+        fn new() -> FakeDirectoryContentsLister {
+            FakeDirectoryContentsLister{}
+        }
+    }
+
+    impl DirectoryContentsLister for FakeDirectoryContentsLister {}
+
+    #[test]
+    fn no_duplicates_for_empty_directory() {
+        let lister = FakeDirectoryContentsLister::new();
+        let duplicates = get_duplicates(&lister);
+        assert_eq!(duplicates.count(), 0);
     }
 }
