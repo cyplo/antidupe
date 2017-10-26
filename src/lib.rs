b287643442eeb9fe5896e0b2f1383164c1c7bc41
Cyryl Płotnicki <cyplo@cyplo.net>
2017-10-26 11:48:00 +0100

    WIP on test for single file

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 mod antidupe {
-    pub trait DirectoryContentsLister {
+
+    pub struct DirectoryEntry {
 
     }
 
-    use std::path::Path;
+    pub trait DirectoryContentsLister {
+        fn entries() -> Vec<&DirectoryEntry>;
+    }
 
     pub struct DuplicatedItem {
-        path: Path
     }
 
-    pub fn get_duplicates(lister: &DirectoryContentsLister) -> Vec<&DuplicatedItem> {
+    pub fn duplicates(lister: &DirectoryContentsLister) -> Vec<&DuplicatedItem> {
         Vec::new()
     }
 
@@ -19,22 +21,38 @@ mod antidupe {
 mod antidupe_should_report {
 
     use antidupe::DirectoryContentsLister;
-    use antidupe::get_duplicates;
+    use antidupe::duplicates;
 
-    struct FakeDirectoryContentsLister { }
+    struct FakeDirectoryContentsLister {
+        entries: Vec<DirectoryEntry>
+    }
 
     impl FakeDirectoryContentsLister {
-        fn new() -> FakeDirectoryContentsLister {
-            FakeDirectoryContentsLister{}
+        fn empty() -> FakeDirectoryContentsLister {
+            FakeDirectoryContentsLister{ entries: Vec::new() }
+        }
+        fn single_file() -> FakeDirectoryContentsLister {
+            FakeDirectoryContentsLister{
+                entries: vec![  ],
+            }
         }
     }
 
-    impl DirectoryContentsLister for FakeDirectoryContentsLister {}
+    impl DirectoryContentsLister for FakeDirectoryContentsLister {
+        fn entries() -> Vec<&DirectoryEntry> {
+            unimplemented!()
+        }
+    }
 
     #[test]
     fn no_duplicates_for_empty_directory() {
-        let lister = FakeDirectoryContentsLister::new();
-        let duplicates = get_duplicates(&lister);
+        let lister = FakeDirectoryContentsLister::empty();
+        let duplicates = duplicates(&lister);
         assert_eq!(duplicates.len(), 0);
+    }
+
+    #[test]
+    fn no_duplicates_for_directory_with_one_file() {
+        let lister = FakeDirectoryContentsLister::single_file();
     }
 }
